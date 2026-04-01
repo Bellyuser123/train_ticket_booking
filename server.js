1ee727374c63921b0f8f6222cdfb5838ad16bcf1
code ka chatora <127492495+Bellyuser123@users.noreply.github.com>
2026-04-01 11:40:36 +0530

    Remove duplicate server listen function

--- server.js
+++ server.js
@@ -29,3 +29,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
