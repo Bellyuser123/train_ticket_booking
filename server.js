325fc4fc7b1a0e139f55e4baa13ce5fe994a5ce5
code ka chatora <127492495+Bellyuser123@users.noreply.github.com>
2026-04-01 11:59:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,5 @@ app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+// this is a comment 
+
